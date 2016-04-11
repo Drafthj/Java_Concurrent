7e523cd2439722710a8c69c5975081af3a26a279
huojia <ahuojiaa@qq.com>
2016-04-11 18:08:52 +0800

    第一章

--- src/chapter_2/Account.java
+++ src/chapter_2/Account.java
@@ -13,4 +13,24 @@ public class Account {
     public void setBalance(double balance) {
         this.balance = balance;
     }
+    public synchronized void addAmount(double amount){
+        double tmp = balance;
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tmp += amount;
+        balance = tmp;
+    }
+    public synchronized void subtractAmount(double amount){
+        double tmp = balance;
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tmp -= amount;
+        balance = tmp;
+    }
 }
---
+++ src/chapter_2/Bank.java
@@ -0,0 +1,12 @@
+package chapter_2;
+
+/**
+ * Created by huojia on 2016/4/11 18:06.
+ */
+public class Bank implements Runnable {
+    private Account account;
+    @Override
+    public void run() {
+
+    }
+}
