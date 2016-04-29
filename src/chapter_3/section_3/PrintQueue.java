b822881bcc1a59994e84aea95f65daa785bb661d
huojia <ahuojiaa@qq.com>
2016-04-29 11:23:38 +0800

    第三章

---
+++ src/chapter_3/section_3/PrintQueue.java
@@ -0,0 +1,54 @@
+package chapter_3.section_3;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by huojia on 2016/4/28 11:07.
+ */
+public class PrintQueue {
+    private boolean freePrinters[];
+    private Lock lockPrinters;
+    private final Semaphore semaphore;
+    public PrintQueue() {
+        this.semaphore = new Semaphore(3);
+        freePrinters = new boolean[3];
+        for(int i = 0;i<freePrinters.length;i++){
+            freePrinters[i] = true;
+        }
+        lockPrinters = new ReentrantLock();
+    }
+    public void printJob(Object document){
+        try {
+            semaphore.acquire();
+            int assignedPrinter = getPrinter();
+            long duration = (long)(Math.random()*10);
+            System.out.printf("%s: PrintQueue: Printing a Job in Printer" +
+                    " %d during %d seconds\n",Thread.currentThread().getName(),assignedPrinter,duration);
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private int getPrinter(){
+        int ret = -1;
+        try {
+            lockPrinters.lock();
+            for(int i = 0;i<freePrinters.length;i++){
+                if(freePrinters[i]){
+                    ret = i;
+                    freePrinters[i] = false;
+                    break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lockPrinters.unlock();
+        }
+        return ret;
+
+    }
+}
